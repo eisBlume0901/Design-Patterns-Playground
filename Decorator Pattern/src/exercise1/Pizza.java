27e551f6819a2c29965267f7a3c1b3c6c0ca5d32
Mary Kate Anecito <marykate09@protonmail.com>
2023-10-31 13:25:14 +0800

    Initial commit

--- Decorator Pattern/src/exercise1/Pizza.java
+++ Decorator Pattern/src/exercise1/Pizza.java
@@ -1,5 +1,7 @@
 package exercise1;
 
+import static java.lang.System.*;
+
 public abstract class Pizza {
     public enum SIZE {
         SMALL, MEDIUM, LARGE, EXTRALARGE;
@@ -8,7 +10,7 @@ public abstract class Pizza {
     private SIZE size = SIZE.SMALL;
     private String description = "Pizza";
 
-    private int cost = 0;
+    private double cost = 0;
 
     public SIZE getSize() {
         return size;
@@ -26,17 +28,23 @@ public abstract class Pizza {
         this.description = description;
     }
 
-    public int getCost() {
+    public double getCost() {
         return cost;
+    }
+
+    public void displayStatus()
+    {
+        out.println(getDescription() + "\n" +
+                "Cost: " + getCost());
     }
 
 }
 
-class PizzaDecorator extends Pizza
+class AddOnIngredients extends Pizza
 {
     private Pizza pizza;
 
-    public PizzaDecorator(Pizza pizza)
+    public AddOnIngredients(Pizza pizza)
     {
         this.pizza = pizza;
     }
@@ -47,7 +55,7 @@ class PizzaDecorator extends Pizza
     }
 
     @Override
-    public int getCost() {
+    public double getCost() {
         return pizza.getCost();
     }
 
@@ -65,8 +73,148 @@ class Margherita extends Pizza
     }
 
     @Override
-    public int getCost() {
-        double cost
-        return super.getCost();
+    public double getCost() {
+        double cost = super.getCost();
+
+        if (super.getSize() == SIZE.SMALL)
+            cost = 300;
+        if (super.getSize() == SIZE.MEDIUM)
+            cost = 400;
+        if (super.getSize() == SIZE.LARGE)
+            cost = 500;
+        if (super.getSize() == SIZE.EXTRALARGE)
+            cost = 600;
+
+        return cost;
+    }
+}
+
+class Pepperoni extends Pizza
+{
+    public Pepperoni()
+    {
+        setDescription("Pepperoni");
+    }
+
+    @Override
+    public double getCost() {
+        double cost = super.getCost();
+
+        if (super.getSize() == SIZE.SMALL)
+            cost = 350;
+        if (super.getSize() == SIZE.MEDIUM)
+            cost = 450;
+        if (super.getSize() == SIZE.LARGE)
+            cost = 550;
+        if (super.getSize() == SIZE.EXTRALARGE)
+            cost = 650;
+
+        return cost;
+    }
+}
+
+class Veggie extends Pizza
+{
+    public Veggie()
+    {
+        setDescription("Veggie");
+    }
+
+    @Override
+    public double getCost() {
+        double cost = super.getCost();
+
+        if (super.getSize() == SIZE.SMALL)
+            cost = 325;
+        if (super.getSize() == SIZE.MEDIUM)
+            cost = 425;
+        if (super.getSize() == SIZE.LARGE)
+            cost = 525;
+        if (super.getSize() == SIZE.EXTRALARGE)
+            cost = 625;
+
+        return cost;
+    }
+}
+class Cheese extends AddOnIngredients
+{
+    public Cheese(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 50;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Cheese";
+    }
+}
+
+class Olives extends AddOnIngredients
+{
+    public Olives(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 40;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Olives";
+    }
+}
+
+class Chicken extends AddOnIngredients
+{
+    public Chicken(Pizza pizza)
+    {
+        super(pizza);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 60;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Chicken";
+    }
+}
+
+class Mushrooms extends AddOnIngredients
+{
+
+    public Mushrooms(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 35;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Mushrooms";
+    }
+}
+class Main
+{
+    public static void main(String[] args)
+    {
+        Pizza margherita = new Margherita();
+        margherita.setSize(Pizza.SIZE.MEDIUM);
+        margherita.displayStatus();
+
+        margherita.setSize(Pizza.SIZE.LARGE);
+        margherita = new Mushrooms(new Olives(new Cheese(new Chicken(margherita))));
+        margherita.displayStatus();
     }
 }
